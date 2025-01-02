1b67252adef2d1038797200d33e5c74e491f0dc9
Karam <qqqqkaramnaser@gmail.com>
2025-01-02 17:21:43 +0300

    update new by karam

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>Palestine Gate</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
@@ -13,25 +13,49 @@
 
         <!-- Scripts -->
         {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
-        
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="" />
+        <meta name="keywords" content="" />
+        <meta name="author" content="" />
+        <link rel="stylesheet" type="text/css" href="{{ url("Frontend/fonts.googleapis.com/css?family=|Roboto+Sans:400,700|Playfair+Display:400,700")}}">
+        <link rel="stylesheet" href="{{ url('Frontend/css/bootstrap.min.css')}}">
+        <link rel="stylesheet" href="{{ url('Frontend/css/animate.css')}}">
+        <link rel="stylesheet" href="{{ url('Frontend/css/owl.carousel.min.css')}}">
+        <link rel="stylesheet" href="{{ url('Frontend/css/aos.css')}}">
+        <link rel="stylesheet" href="{{ url('Frontend/css/bootstrap-datepicker.css')}}">
+        <link rel="stylesheet" href="{{ url('Frontend/css/jquery.timepicker.css')}}">
+        <link rel="stylesheet" href="{{ url('Frontend/css/fancybox.min.css')}}">
+        <link rel="stylesheet" href="{{ url('Frontend/fonts/ionicons/css/ionicons.min.css')}}">
+        <link rel="stylesheet" href="{{ url('Frontend/fonts/fontawesome/css/font-awesome.min.css')}}">
+        <link rel="stylesheet" href="{{ url('Frontend/css/style.css')}}">
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
             @include('layouts.navigation')
 
-            <!-- Page Heading -->
-            @isset($header)
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endisset
 
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
+           @yield('content')
+
+           @include('layouts.footer')
         </div>
+
+        <script src="{{ url('Frontend/js/jquery-3.3.1.min.js') }}"></script>
+        <script src="{{ url('Frontend/js/jquery-migrate-3.0.1.min.js') }}"></script>
+        <script src="{{ url('Frontend/js/popper.min.js') }}"></script>
+        <script src="{{ url('Frontend/js/bootstrap.min.js')}}"></script>
+        <script src="{{ url('Frontend/js/owl.carousel.min.js')}}"></script>
+        <script src="{{ url('Frontend/js/jquery.stellar.min.js')}}"></script>
+        <script src="{{ url('Frontend/js/jquery.fancybox.min.js')}}"></script>
+
+
+        <script src="{{ url('Frontend/js/aos.js')}}"></script>
+
+        <script src="{{ url('Frontend/js/bootstrap-datepicker.js')}}"></script>
+        <script src="{{ url('Frontend/js/jquery.timepicker.min.js')}}"></script>
+
+
+
+        <script src="{{ url('Frontend/js/main.js')}}"></script>
+
     </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,72 +1,16 @@
-<!DOCTYPE HTML>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Palestine Gate</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="" />
-    <meta name="keywords" content="" />
-    <meta name="author" content="" />
-    <link rel="stylesheet" type="text/css" href="{{ url("Frontend/fonts.googleapis.com/css?family=|Roboto+Sans:400,700|Playfair+Display:400,700")}}">
-    <link rel="stylesheet" href="{{ url('Frontend/css/bootstrap.min.css')}}">
-    <link rel="stylesheet" href="{{ url('Frontend/css/animate.css')}}">
-    <link rel="stylesheet" href="{{ url('Frontend/css/owl.carousel.min.css')}}">
-    <link rel="stylesheet" href="{{ url('Frontend/css/aos.css')}}">
-    <link rel="stylesheet" href="{{ url('Frontend/css/bootstrap-datepicker.css')}}">
-    <link rel="stylesheet" href="{{ url('Frontend/css/jquery.timepicker.css')}}">
-    <link rel="stylesheet" href="{{ url('Frontend/css/fancybox.min.css')}}">  
-    <link rel="stylesheet" href="{{ url('Frontend/fonts/ionicons/css/ionicons.min.css')}}">
-    <link rel="stylesheet" href="{{ url('Frontend/fonts/fontawesome/css/font-awesome.min.css')}}">
-    <link rel="stylesheet" href="{{ url('Frontend/css/style.css')}}">
-  </head>
-  <body>
-    
-    <header class="site-header js-site-header">
-      <div class="container-fluid">
-        <div class="row align-items-center">
-          <div class="col-6 col-lg-4 site-logo" data-aos="fade"><a href="index.html">Palestine Gate</a></div>
-          <div class="col-6 col-lg-8">
+@extends('layouts.app')
+@section('content')
 
 
-            <div class="site-menu-toggle js-site-menu-toggle"  data-aos="fade">
-              <span></span>
-              <span></span>
-              <span></span>
-            </div>
-            <!-- END menu-toggle -->
 
-            <div class="site-navbar js-site-navbar">
-              <nav role="navigation">
-                <div class="container">
-                  <div class="row full-height align-items-center">
-                    <div class="col-md-6 mx-auto">
-                      <ul class="list-unstyled menu">
-                        <li class="active"><a href="index.html">Home</a></li>
-                        <li><a href="Services.html">Services</a></li>
-                        <li><a href="AboutUs.html">About Us</a></li>
-                        <li><a href="ContactUs.html">Contact Us</a></li>
-                        <li><a href="Login.html">LogIn</a></li>
-                        <li><a href="Register.html">Register</a></li>
-                      </ul>
-                    </div>
-                  </div>
-                </div>
-              </nav>
-            </div>
-          </div>
-        </div>
-      </div>
-    </header>
     <!-- END head -->
-
-    <section class="site-hero overlay" style="background-image: {{(url('Frontend/images/Jerusalem.jpg'))}}" data-stellar-background-ratio="0.5">
-      <div class="container">
+    <section class="site-hero overlay" style="background-image: url('{{ asset('Frontend/images/Jerusalem.jpg') }}');">
+          <div class="container">
         <div class="row site-hero-inner justify-content-center align-items-center">
           <div class="col-md-10 text-center" data-aos="fade-up">
             <span class="custom-caption text-uppercase text-white d-block  mb-3">Welcome To Palestine Gate <span class="fa fa-star text-primary">
             <h1 class="heading">Find your best stay</h1>
-          </div>
+          </div>{
         </div>
       </div>
 
@@ -80,7 +24,7 @@
 
     <section class="section bg-light pb-0"  >
       <div class="container">
-       
+
         <div class="row check-availabilty" id="next">
           <div class="block-32" data-aos="fade-up" data-aos-offset="-200">
 
@@ -154,7 +98,7 @@
             <p class="mb-4">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
             <p><a href="#" class="btn btn-primary text-white py-2 mr-3">Learn More</a> <span class="mr-3 font-family-serif"><em>or</em></span> <a href="https://vimeo.com/channels/staffpicks/93951774"  data-fancybox class="text-uppercase letter-spacing-1">See video</a></p>
           </div>
-          
+
         </div>
       </div>
     </section>
@@ -208,8 +152,8 @@
         </div>
       </div>
     </section>
-    
-    
+
+
     <section class="section slider-section bg-light">
       <div class="container">
         <div class="row justify-content-center text-center mb-5">
@@ -245,12 +189,12 @@
             </div>
             <!-- END slider -->
           </div>
-        
+
         </div>
       </div>
     </section>
     <!-- END section -->
-    
+
     <section class="section bg-image overlay" style="background-image: url('images/hero_3.jpg');">
       <div class="container">
         <div class="row justify-content-center text-center mb-5">
@@ -272,8 +216,8 @@
             </li>
           </ul>
           <div class="tab-content py-5" id="myTabContent">
-            
-            
+
+
             <div class="tab-pane fade show active text-left" id="mains" role="tabpanel" aria-labelledby="mains-tab">
               <div class="row">
                 <div class="col-md-6">
@@ -311,7 +255,7 @@
                   </div>
                 </div>
               </div>
-              
+
             </div> <!-- .tab-pane -->
 
             <div class="tab-pane fade text-left" id="desserts" role="tabpanel" aria-labelledby="desserts-tab">
@@ -394,7 +338,7 @@
         </div>
       </div>
     </section>
-    
+
     <!-- END section -->
     <section class="section testimonial-section">
       <div class="container">
@@ -405,7 +349,7 @@
         </div>
         <div class="row">
           <div class="js-carousel-2 owl-carousel mb-5" data-aos="fade-up" data-aos-delay="200">
-            
+
             <div class="testimonial text-center slider-item">
               <div class="author-image mb-3">
                 <img src="images/person_1.jpg" alt="Image placeholder" class="rounded-circle mx-auto">
@@ -415,7 +359,7 @@
                 <p>&ldquo;A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.&rdquo;</p>
               </blockquote>
               <p><em>&mdash; Jean Smith</em></p>
-            </div> 
+            </div>
 
             <div class="testimonial text-center slider-item">
               <div class="author-image mb-3">
@@ -448,7 +392,7 @@
                 <p>&ldquo;A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.&rdquo;</p>
               </blockquote>
               <p><em>&mdash; Jean Smith</em></p>
-            </div> 
+            </div>
 
             <div class="testimonial text-center slider-item">
               <div class="author-image mb-3">
@@ -477,7 +421,7 @@
 
       </div>
     </section>
-    
+
 
     <section class="section blog-post-entry bg-light">
       <div class="container">
@@ -537,75 +481,9 @@
         </div>
       </section>
 
-    <footer class="section footer-section">
-      <div class="container">
-        <div class="row mb-4">
-          <div class="col-md-3 mb-5">
-            <ul class="list-unstyled link">
-              <li><a href="#">About Us</a></li>
-              <li><a href="#">Terms &amp; Conditions</a></li>
-              <li><a href="#">Privacy Policy</a></li>
-             <li><a href="#">Rooms</a></li>
-            </ul>
-          </div>
-          <div class="col-md-3 mb-5">
-            <ul class="list-unstyled link">
-              <li><a href="#">The Rooms &amp; Suites</a></li>
-              <li><a href="#">About Us</a></li>
-              <li><a href="#">Contact Us</a></li>
-              <li><a href="#">Restaurant</a></li>
-            </ul>
-          </div>
-          <div class="col-md-3 mb-5 pr-md-5 contact-info">
-            <!-- <li>198 West 21th Street, <br> Suite 721 New York NY 10016</li> -->
-            <p><span class="d-block"><span class="ion-ios-location h5 mr-3 text-primary"></span>Address:</span> <span> 198 West 21th Street, <br> Suite 721 New York NY 10016</span></p>
-            <p><span class="d-block"><span class="ion-ios-telephone h5 mr-3 text-primary"></span>Phone:</span> <span> (+1) 435 3533</span></p>
-            <p><span class="d-block"><span class="ion-ios-email h5 mr-3 text-primary"></span>Email:</span> <span> info@domain.com</span></p>
-          </div>
-          <div class="col-md-3 mb-5">
-            <p>Sign up for our newsletter</p>
-            <form action="#" class="footer-newsletter">
-              <div class="form-group">
-                <input type="email" class="form-control" placeholder="Email...">
-                <button type="submit" class="btn"><span class="fa fa-paper-plane"></span></button>
-              </div>
-            </form>
-          </div>
-        </div>
-        <div class="row pt-5">
-          <p class="col-md-6 text-left">
-            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-            Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="icon-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank" >Colorlib</a>
-            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-          </p>
-            
-          <p class="col-md-6 text-right social">
-            <a href="#"><span class="fa fa-tripadvisor"></span></a>
-            <a href="#"><span class="fa fa-facebook"></span></a>
-            <a href="#"><span class="fa fa-twitter"></span></a>
-            <a href="#"><span class="fa fa-linkedin"></span></a>
-            <a href="#"><span class="fa fa-vimeo"></span></a>
-          </p>
-        </div>
-      </div>
-    </footer>
-    
-    <script src="{{ url('Frontend/js/jquery-3.3.1.min.js') }}"></script>
-    <script src="{{ url('Frontend/js/jquery-migrate-3.0.1.min.js') }}"></script>
-    <script src="{{ url('Frontend/js/popper.min.js') }}"></script>
-    <script src="{{ url('Frontend/js/bootstrap.min.js')}}"></script>
-    <script src="{{ url('Frontend/js/owl.carousel.min.js')}}"></script>
-    <script src="{{ url('Frontend/js/jquery.stellar.min.js')}}"></script>
-    <script src="{{ url('Frontend/js/jquery.fancybox.min.js')}}"></script>
-    
-    
-    <script src="{{ url('Frontend/js/aos.js')}}"></script>
-    
-    <script src="{{ url('Frontend/js/bootstrap-datepicker.js')}}"></script> 
-    <script src="{{ url('Frontend/js/jquery.timepicker.min.js')}}"></script> 
 
-    
 
-    <script src="{{ url('Frontend/js/main.js')}}"></script>
+
   </body>
 </html>
+@endsection
