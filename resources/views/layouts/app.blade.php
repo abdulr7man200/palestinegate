e6a48242a11b1850cd7b274a13f4a9a1f6bcabb1
abdulrhman nihad <abdulrhman.n.naser@gmail.com>
2025-01-04 18:13:11 +0200

    new pages v2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,8 +11,6 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-        <!-- Scripts -->
-        {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta name="description" content="" />
         <meta name="keywords" content="" />
