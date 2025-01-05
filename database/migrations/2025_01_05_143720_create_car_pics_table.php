e9fa6f7344a3c49a6cf471e30434cc601e8eaf79
Karam <qqqqkaramnaser@gmail.com>
2025-01-05 18:16:10 +0300

    uds

--- database/migrations/2025_01_05_143720_create_car_pics_table.php
+++ database/migrations/2025_01_05_143720_create_car_pics_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('propety_pics', function (Blueprint $table) {
+        Schema::create('propety_pics', function (Blueprint $table): void {
             $table->id();
             $table->string('path');
             $table->unsignedBigInteger("car_id");
