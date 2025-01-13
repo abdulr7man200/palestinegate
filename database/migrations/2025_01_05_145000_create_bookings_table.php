9a16bbdca5b710fa317b75981b97d1c7ab7e7725
abdulrhman nihad <abdulrhman.n.naser@gmail.com>
2025-01-13 22:47:35 +0200

    random

--- database/migrations/2025_01_05_145000_create_bookings_table.php
+++ database/migrations/2025_01_05_145000_create_bookings_table.php
@@ -27,7 +27,6 @@ return new class extends Migration
             $table->foreign('stay_id')->references('id')->on('stays')->onDelete('cascade');
             $table->foreign('car_id')->references('id')->on('cars')->onDelete('cascade');
             $table->timestamps();
-
         });
     }
 
